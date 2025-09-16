5bef1b8041a0b5577f324ff464bf2a4bab4122af
priyabridgingFX <priyabridgingfx@gmail.com>
2025-09-17 01:02:40 +0400

    committing changes

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useLanguage } from "../../context/LanguageContext";
 
 // Menu structure
